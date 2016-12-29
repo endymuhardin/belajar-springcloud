5f4e4ee8b88e66bc0cd8c0d69d737617aa6ff6d2
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-30 00:16:13 +0700

    registrasi config server ke eureka

--- config-server/src/main/java/com/muhardin/endy/belajar/springcloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/muhardin/endy/belajar/springcloud/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.muhardin.endy.belajar.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
