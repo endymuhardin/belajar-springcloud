6aeaeafab1fb13779db395a296123fd9378592cb
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-30 00:29:35 +0700

    lookup config server dari eureka

--- catalog-service/src/main/java/com/muhardin/endy/belajar/springcloud/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/muhardin/endy/belajar/springcloud/CatalogServiceApplication.java
@@ -6,10 +6,12 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RestController
 public class CatalogServiceApplication {
 
