c478bc2e0f90b127dd9d45dc97fc10624dd83125
Endy Muhardin <endy.muhardin@gmail.com>
2017-01-03 22:31:06 +0700

    fix template url

--- online-store/src/main/java/com/muhardin/endy/belajar/springcloud/onlinestore/controller/ProductController.java
+++ online-store/src/main/java/com/muhardin/endy/belajar/springcloud/onlinestore/controller/ProductController.java
@@ -32,6 +32,6 @@ public class ProductController {
             System.out.println("Jumlah foto  : " +photos.size());
             mm.addAttribute("daftarFoto", photos);
         }
-        return "/product/detail";
+        return "product/detail";
     }
 }
