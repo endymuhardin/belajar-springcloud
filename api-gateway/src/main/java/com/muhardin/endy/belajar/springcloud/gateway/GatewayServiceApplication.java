3c4b1e78691826c20e303b346c63d92169206751
Endy Muhardin <endy.muhardin@gmail.com>
2017-01-08 00:09:41 +0700

    fix nama package

--- api-gateway/src/main/java/com/muhardin/endy/belajar/springcloud/gateway/GatewayServiceApplication.java
+++ api-gateway/src/main/java/com/muhardin/endy/belajar/springcloud/gateway/GatewayServiceApplication.java
@@ -1,4 +1,4 @@
-package com.muhardin.endy.belajar.springcloud.image;
+package com.muhardin.endy.belajar.springcloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
