165e11a7ad682838134fb134a9654a26135c69cb
Endy Muhardin <endy.muhardin@gmail.com>
2017-01-08 01:01:58 +0700

    implement oauth sso

---
+++ api-gateway/src/main/java/com/muhardin/endy/belajar/springcloud/gateway/KonfigurasiSecurity.java
@@ -0,0 +1,18 @@
+package com.muhardin.endy.belajar.springcloud.gateway;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@EnableWebSecurity(debug = true)
+@EnableOAuth2Sso
+public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**").authorizeRequests().anyRequest().authenticated()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+}
